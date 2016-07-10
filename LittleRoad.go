41e68e51d138ab4147e9c5f1f5f717ec0d3a5d58
grantyuan <grant.yuan@waterfrontech.com>
2016-07-10 23:39:22 +0800

    code changed , not able to be compiled

--- LittleRoad.go
+++ LittleRoad.go
@@ -14,6 +14,7 @@ import (
 type LittleRoadClientConfig struct {
 	LocalAddr  string
 	RemoteAddr string
+	ServerAddr string
 }
 
 func main() {
@@ -48,6 +49,25 @@ func main() {
 		fmt.Println("Failed to open local port to listen: %s", err)
 		os.Exit(5)
 	}
+
+	//	serverAddrs, err := net.LookupHost("www.waterfrontech.com")
+	//	if err != nil {
+	//		fmt.Println("unable to resovle dns for server address", "www.waterfrontech.com")
+	//		os.Exit(6)
+	//	}
+	var serverAdr string
+	if clientConfig.ServerAddr != nil {
+		serverAdr := clientConfig.ServerAddr
+	} else {
+		fmt.Println("unable get server address")
+		os.Exit(6)
+	}
+
+	saddr, err := net.ResolveTCPAddr("tcp", serverAdr)
+	if err != nil {
+		fmt.Println("failed to resolve tcp address", serverAdr)
+	}
+
 	connid := 0
 	for {
 		conn, err := listener.AcceptTCP()
@@ -56,12 +76,24 @@ func main() {
 			continue
 		}
 		connid++
-		fmt.Println("connection:", connid)
+		fmt.Println("connection:", connid, " connect to", raddr.String())
 		var p = tcpproxy.New(conn, laddr, raddr)
+
+		p.Nagles = true
 
 		go p.Start()
 	}
 
 	//tcpproxy.n
 	//tcpproxy.NewTLSUnwrapped()
+}
+
+func CreateNewProxy(conn *net.TCPConn, raddr *net.TCPAddr) error {
+	//1. connect to server
+	//2. tell server my name
+	//3. server make a table, you name
+	//4. client connect to server:8080,
+	//5. client how to connect to the real server?.
+
+	//TODO
 }
